45a7c8bf7665ab5eb89de896bf3ba2e347591075
Brendan <brendanmcgaw@gmail.com>
2025-07-27 20:09:11 +1000

    Stores average, highest and lowest grades and then checks to see if each student has passed the class or not.

--- src/App.java
+++ src/App.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {\
+    public static void main(String[] args) throws Exception {
         // instantiate our variables.
         Scanner keyboard = new Scanner(System.in);
         int numberOfStudents = 0;
         
+        // Try catch to make sure we catch any unexpected exceptions. Takes user input to assign how many students there are in the class.
         try {
             System.out.print("Please enter the number of students: ");
             numberOfStudents = keyboard.nextInt();
@@ -38,9 +39,38 @@ public class App {
             }
         }
 
-        // for (int grade : grades) {
-        //     System.out.println(grade);
-        // }
+        // variables to record total sum, highest and lowest grades.
+        int sum = 0;
+        int highestGrade = grades[0];
+        int lowestGrade = grades[0];
+
+        // loop to assign values to the sum, highest and lowest grades.
+        for (int grade: grades) {
+            sum += grade;
+            if (grade > highestGrade) {
+                highestGrade = grade;
+            }
+            else if (grade < lowestGrade) {
+                lowestGrade = grade;
+            }
+        }
+
+        // Finds the average grade score based on the sum, divided by the number of students.
+        double average = (double) sum / numberOfStudents;
+        System.out.println("Average grade: " + average);
+        System.out.println("Highest grade: " + highestGrade);
+        System.out.println("Lowest grade: " + lowestGrade);
+
+
+        // Checks to see if the student has passed the class with a grade of 50 or more.
+        for (int i = 0; i < numberOfStudents; i++) {
+            if (grades[i] >= 50) {
+                System.out.println("Student " + (i + 1) + " has passed the class with a grade of " + grades[i]);
+            }
+            else {
+                System.out.println("Student " + (i + 1) + " has failed the class with a grade of " + grades[i]);
+            }
+        }
 
         keyboard.close();
     }
